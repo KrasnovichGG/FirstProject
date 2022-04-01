76d9c7231d739769bc10e2827fd892c139801727
Alexunder <email@example.com>
2022-04-01 09:00:26 +0300

    MVVMAplusLoginModel

---
+++ FirstProject/FirstProject/ViewModel/LoginModelView.cs
@@ -0,0 +1,70 @@
+﻿using FirstProject.models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace FirstProject.ViewModel
+{
+    public class LoginModelView : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ICommand LoginCommand { protected set; get; }
+        public ICommand RegistrationCommand { protected set; get; }
+
+        public INavigation Navigation { get; set; }
+        public Client Client { get; private set; } = new Client("", "", "");
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public string Login
+        {
+            get { return Client.Login; }
+            set
+            {
+                Client.Login = value;
+                OnPropertyChanged("Login");
+            }
+        }
+        public string Password
+        {
+            get { return Client.Password; }
+            set
+            {
+                Client.Password = value;
+                OnPropertyChanged("Password");
+            }
+        }
+
+        private async void RegistrationPageBtn()
+        {
+            await Navigation.PushAsync(new RegistrationPage());
+        }
+
+        private async void LoginBtn()
+        {
+            var lst = App.Db.GetClients();
+            bool state = false;
+
+            foreach (var item in lst)
+            {
+                if (item.Login == Client.Login)
+                {
+                    if (item.Password == Client.Password && state == false)
+                    {
+                        state = true;
+                        await Navigation.PushModalAsync(new NavigationPage(new MainPage()));
+                    }
+                }
+            }
+
+            if (!state)
+                throw new Exception("Уведомление Не правилный логин или пароль");
+        }
+    }
+}
+
